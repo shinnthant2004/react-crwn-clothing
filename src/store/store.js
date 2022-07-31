81b2d597cb4d17aecd6ef150708b4cf2c846b937
shinnthant2004 <shinthantequi@gmail.com>
2022-07-31 16:08:58 +0630

    custom middleware

--- src/store/store.js
+++ src/store/store.js
@@ -3,19 +3,17 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-// const loggerMiddleWares = (store) => (next) => (action) => {
-//   if (!action.type) {
-//     return next(action);
-//   }
-//   console.log("type: ", action.type);
-//   console.log("payload: ", action.payload);
-//   console.log("currentState: ", store.getState());
+// Custom middleware
+const loggerMiddleWares = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
 
-//   next(action);
-//   console.log("next state: ", store.getState());
-// };
-const loggerMiddleWares = (action) => {
-  console.log(action);
+  next(action);
+  console.log("next state: ", store.getState());
 };
 
 const middleWares = [loggerMiddleWares];
